dfd865a9799c979e254c7cc2b47eb0b1b11b5f5d
Seungwoo321 <seungwoo321@gmail.com>
2026-02-19 02:51:56 +0900

    refactor: extract drag and resize hooks
    GridItem과 ResizeHandle의 드래그 및 리사이즈 로직을 useDrag, useResize 커스텀 훅으로 분리했습니다. 컴포넌트 복잡도를 낮추고 로직 재사용성을 향상시켰습니다.

---
+++ src/hooks/index.ts
@@ -0,0 +1,5 @@
+export { useResize } from './useResize'
+export type { UseResizeOptions, UseResizeReturn } from './useResize'
+
+export { useDrag } from './useDrag'
+export type { UseDragOptions, UseDragReturn } from './useDrag'
